42e5722cd793a0f1de94bfc036db0379768805c8
Vrinda Dixit <dixitvrinda1704@gmail.com>
2025-08-21 20:58:50 +0530

    Initial commit: Express + Mongo setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(() => {
+        console.log("connected to db");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+async function main(params) {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+    res.send("im root");
+})
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+})
