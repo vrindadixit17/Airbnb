edfa2213fc0dd94b03ea024e4469cdb303e8de53
Vrinda Dixit <dixitvrinda1704@gmail.com>
2025-08-29 19:13:23 +0530

    finalised init

--- init/index.js
+++ init/index.js
@@ -1,37 +1,25 @@
 const mongoose = require("mongoose");
-const data = require("./data.js");
+const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
     .then(() => {
-        console.log("connected to db");
+        console.log("connected to DB");
     })
     .catch((err) => {
         console.log(err);
     });
 
-async function main(params) {
+async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    await Listing.insertMany(data.data);
-    console.log("data was initialised")
-}
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+};
 
-main()
-    .then(() => {
-        console.log("connected to db");
-        // Call the initialization function after a successful connection
-        return initDB();
-    })
-    .then(() => {
-        console.log("Database initialization complete!");
-    })
-    .catch((err) => {
-        console.log("An error occurred:");
-        console.log(err);
-    });
+initDB();
