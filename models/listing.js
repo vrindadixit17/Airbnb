c207403ed778ec3b32df72bbc7f56f7e67052959
Vrinda Dixit <dixitvrinda1704@gmail.com>
2025-08-29 19:14:26 +0530

    finalised models

--- models/listing.js
+++ models/listing.js
@@ -9,16 +9,14 @@ const listingSchema = new Schema({
     description: String,
     image: {
         type: String,
-        default: "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60",
+        default:
+            "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
         set: (v) =>
             v === ""
-                ? "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60"
+                ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
                 : v,
     },
-    price: {
-        type: Number,
-        min: 0,
-    },
+    price: Number,
     location: String,
     country: String,
 });
